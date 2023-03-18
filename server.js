c5ed9c378b9ae0706c80f1f3be66260fa65f7b86
Krishnay mudgal <kmud2208@gmail.com>
2023-03-18 11:40:56 +0530

    views

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(express.json());
 
 // Set the view engine
 app.set('view engine', 'ejs');
-
+app.set('views', __dirname + '/views');
 // Serve static files from the public directory
 app.use(express.static('public'));
 
