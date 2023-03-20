9fe2db94e166232b45888eaa10d6ac7e1603f4bd
Krishnay mudgal <kmud2208@gmail.com>
2023-03-20 10:03:37 +0530

    removed popup

--- server.js
+++ server.js
@@ -1,115 +1,130 @@
 //require('dotenv').config();
 const express = require("express");
 const colors = require("colors");
-const app= express();
-const admin = require('firebase-admin');
+const app = express();
 const bodyParser = require('body-parser');
+
+const admin = require('firebase-admin');
+const firebase = require('firebase/app');
+
+require('firebase/auth');
+require('firebase/firestore');
 app.use(bodyParser.urlencoded({ extended: true }));
+const serviceAccount = require('./hackathon-c3b03-firebase-adminsdk-vxv9h-c3285e1205.json');
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: 'https://hackathon-c3b03-default-rtdb.firebaseio.com',
+
+});
 // app.use(express.json());
 
 // Set the view engine
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');   //for deployment
 // Serve static files from the public directory
-app.use(express.static(__dirname+"/public/"));    // add this to make sure public folder is read on deployment
+app.use(express.static(__dirname + "/public/"));    // add this to make sure public folder is read on deployment
 
 const PORT = process.env.PORT || 3000;
 
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
     // alert("Hello Its popup");
-    res.render("index.ejs");
+    // const user = firebase.auth.currentUser; 
+    // // // console.log(user);
+    // if (user) {
+    // //     // // User is signed in, so display their details
+    //      console.log(typeof(user));
+    //      const name = user.name;
+    // //     // // const  = user.uid;
+    //      res.render("index.ejs", { currUserName:name });
+    //  } else {
+    // //     // // User is not signed in
+    //     res.render("index.ejs");
+    //  }
+     res.render("index.ejs");
 })
-app.post("/" , (req,res)=>{
-   res.render("index.ejs");
+app.post("/", (req, res) => {
+    res.render("index.ejs");
 });
 
-app.get("/login" , (req,res)=>{
+app.get("/login", (req, res) => {
     res.render("login.ejs");
 });
 
-app.get("/signup" ,(req,res)=>{
+app.get("/signup", (req, res) => {
     res.render("signup.ejs");
 });
 
-app.post("/signup", (req,res)=>{
-    console.log(req);
-    const { name, password,confirmpassword, phoneno, aadharno, gender, age, state, income } = req.body;
-    console.log("HI my name is " , name);
+app.post("/signup", (req, res) => {
+    const { name, password, confirmpassword, phoneno, aadharno, gender, age, state, income } = req.body;
     if (password !== confirmpassword) {
         return res.status(400).send('Passwords do not match');
-      }
-      admin.auth().createUser({
+    }
+    admin.auth().createUser({
         displayName: name,
         password: password
-      })
-      .then((userRecord) => {
-        // Store additional user data in Firebase Realtime Database
-        const userData = {
-          name,
-          phoneno,
-          aadharno,
-          gender,
-          age: parseInt(age),
-          state,
-          income: parseInt(income)
-        };
-        // console.log(userData);
-        return admin.database().ref(`users/${userRecord.uid}`).set(userData);
-      })
+    })
+        .then((userRecord) => {
+            // Store additional user data in Firebase Realtime Database
+            const userData = {
+                name,
+                phoneno,
+                aadharno,
+                gender,
+                age: parseInt(age),
+                state,
+                income: parseInt(income)
+            };
+             console.log(userData);
+            return admin.database().ref(`users/${userRecord.uid}`).set(userData);
+        })
 
-      .then(() => {
-        console.log("User Registered");
-        console.log(userData);
-        res.status(200).render("/")
-      })
+        .then(() => {
+            console.log("User Registered");
+            // console.log(userData);
+            res.redirect('/');
+        })
 
-      .catch((error) => {
-        console.error(error);
-        res.status(500).send('An error occurred');
-      });
-    });
-    
-app.get("/education" ,(req,res)=>{
+        .catch((error) => {
+            console.error(error);
+            res.status(500).send('An error occurred');
+        });
+});
+
+app.get("/education", (req, res) => {
     res.render("education.ejs");
 });
-app.get("/electricity" ,(req,res)=>{
+app.get("/electricity", (req, res) => {
     res.render("electricity.ejs");
 });
-app.get("/health" ,(req,res)=>{
+app.get("/health", (req, res) => {
     res.render("health.ejs");
 });
-app.get("/water" ,(req,res)=>{
+app.get("/water", (req, res) => {
     res.render("water.ejs");
 });
-app.get("/money" ,(req,res)=>{
+app.get("/money", (req, res) => {
     res.render("money.ejs");
 });
-app.get("/jobs" ,(req,res)=>{
+app.get("/jobs", (req, res) => {
     res.render("generic.ejs");
 });
-app.get("/justice" ,(req,res)=>{
+app.get("/justice", (req, res) => {
     res.render("generic.ejs");
 });
-app.get("/law" ,(req,res)=>{
+app.get("/law", (req, res) => {
     res.render("generic.ejs");
 });
-app.get("/buisness" ,(req,res)=>{
+app.get("/buisness", (req, res) => {
     res.render("generic.ejs");
 });
-app.get("/pension" ,(req,res)=>{
+app.get("/pension", (req, res) => {
     res.render("generic.ejs");
 });
-app.get("/agriculture" ,(req,res)=>{
+app.get("/agriculture", (req, res) => {
     res.render("generic.ejs");
 });
-app.get("/rural" ,(req,res)=>{
+app.get("/rural", (req, res) => {
     res.render("generic.ejs");
 });
 
-const serviceAccount = require('./hackathon-c3b03-firebase-adminsdk-vxv9h-c3285e1205.json');
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: 'https://hackathon-c3b03-default-rtdb.firebaseio.com',
-
-});
 app.listen(PORT, console.log(`Server running on port ${PORT}`.yellow.bold));
